e7c29dde73022befac0d00fdc226c7eea1dfc485
MonjurElahiTouheed <monjurelahitouheed@gmail.com>
2025-03-22 16:33:27 +0600

    remove button script written

--- app.js
+++ app.js
@@ -4,6 +4,11 @@ addBtn.addEventListener('click', () => {
     const todoField = document.getElementById('todo__field');
     console.log(todoField.value);
     const todoItem = document.createElement('li');
-    todoItem.innerHTML =`${todoField.value} <button onClick='removeItem()'>Remove</button>`
+    todoItem.innerHTML =`<input type='checkbox'/> ${todoField.value} <button onClick='removeItem(this)'>Remove</button>`
     todoList.appendChild(todoItem);
 })
+
+function removeItem(button) {
+    const li = button.parentNode;
+    li.remove();
+}
