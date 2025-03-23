3b09fc20faac5c116e749cd29e5993d9c00923cf
MonjurElahiTouheed <monjurelahitouheed@gmail.com>
2025-03-23 14:39:26 +0600

    filter button logic in progress

--- app.js
+++ app.js
@@ -30,3 +30,21 @@ function deleteAll(){
         checkbox.remove();
     })
 }
+
+const filterButton = document.querySelector('.filter_button');
+function showfilterBtn(){
+            filterButton.style.visibility='visible';
+}
+function hidefilterBtn(){
+        filterButton.style.visibility='hidden';
+}
+
+
+function filter() {
+    const checkboxes = document.querySelectorAll('input[type=checkbox]');
+    checkboxes.forEach( checkbox => {
+        if(!checkbox.checked){
+            todoList.removeChild(checkbox.parentElement);
+        }
+    })
+}
