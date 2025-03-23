578737adfaa530e0d46b43e0c9c2fb88a7d56783
MonjurElahiTouheed <monjurelahitouheed@gmail.com>
2025-03-23 17:14:12 +0600

    total task counted

--- app.js
+++ app.js
@@ -1,11 +1,28 @@
 const addBtn = document.getElementById('addBtn');
 const todoList = document.querySelector('.todo_list');
+const completed = document.querySelector('.completed');
+const total = document.querySelector('.total');
+total.innerText = todoList.childElementCount;
+completed.innerText = completed.childElementCount;
+const checkboxInputs = document.querySelectorAll('input[type=checkbox]');
+
 addBtn.addEventListener('click', () => {
     const todoField = document.getElementById('todo__field');
-    console.log(todoField.value);
-    const todoItem = document.createElement('li');
+    if(todoField.value === '' || todoField.value === ''){
+        alert('Please vai kisu den,, eid er somoy khali haate firaiyen na');
+    }
+    else {
+        const todoItem = document.createElement('li');
     todoItem.innerHTML =`<input type='checkbox'/> ${todoField.value} <button onClick='removeItem(this)'>Remove</button>`
     todoList.appendChild(todoItem);
+    todoField.value = '';
+    }
+    
+    total.innerText = todoList.childElementCount;
+
+    checkboxInputs.forEach(checkbox => {
+        completed.innerText = 
+    })
 })
 
 function removeItem(button) {
@@ -35,15 +52,24 @@ const filterButton = document.querySelector('.filter_button');
 function showfilterBtn(){
             filterButton.style.visibility='visible';
 }
+
 function hidefilterBtn(){
         filterButton.style.visibility='hidden';
 }
 
 
-function filter() {
+function marked() {
     const checkboxes = document.querySelectorAll('input[type=checkbox]');
     checkboxes.forEach( checkbox => {
         if(!checkbox.checked){
+            todoList.removeChild(checkbox.parentElement);
+        }
+    })
+}
+function unmarked() {
+    const checkboxes = document.querySelectorAll('input[type=checkbox]');
+    checkboxes.forEach( checkbox => {
+        if(checkbox.checked){
             todoList.removeChild(checkbox.parentElement);
         }
     })
