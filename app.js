955665577d64659d4f71f7f21f88eb83efa843e0
MonjurElahiTouheed <monjurelahitouheed@gmail.com>
2025-03-27 14:56:27 +0600

    completed count showed

--- app.js
+++ app.js
@@ -3,74 +3,106 @@ const todoList = document.querySelector('.todo_list');
 const completed = document.querySelector('.completed');
 const total = document.querySelector('.total');
 total.innerText = todoList.childElementCount;
-completed.innerText = completed.childElementCount;
-const checkboxInputs = document.querySelectorAll('input[type=checkbox]');
+let count = 0
+completed.innerText = count;
+const checkboxes = document.querySelectorAll('input[type=checkbox]');
 
-addBtn.addEventListener('click', () => {
+addBtn.addEventListener("click", function () {
     const todoField = document.getElementById('todo__field');
-    if(todoField.value === '' || todoField.value === ''){
+    if (todoField.value === '' || todoField.value === '') {
         alert('Please vai kisu den,, eid er somoy khali haate firaiyen na');
     }
     else {
         const todoItem = document.createElement('li');
-    todoItem.innerHTML =`<input type='checkbox'/> ${todoField.value} <button onClick='removeItem(this)'>Remove</button>`
-    todoList.appendChild(todoItem);
-    todoField.value = '';
+        todoItem.innerHTML = `<input onclick='completedFunc()' type='checkbox'/> ${todoField.value} <button onClick='removeItem(this)'>Remove</button>`;
+        todoList.appendChild(todoItem);
+        todoField.value = '';
     }
-    
+
     total.innerText = todoList.childElementCount;
 
-    checkboxInputs.forEach(checkbox => {
-        completed.innerText = 
-    })
+    // const checkboxes = document.querySelectorAll('input[type=checkbox]');
+    // checkboxes.forEach(checkbox => {
+    //     checkbox.addEventListener('click', () => {
+    //         const checked = document.querySelectorAll('input[type=checkbox]:checked');
+    //         completed.innerText = checked.length;
+    //         //    console.log(checked);
+    //         console.log(checked.length);
+    //     })
+    // })
 })
+
+
 
 function removeItem(button) {
     const li = button.parentNode;
     li.remove();
+    total.innerText = todoList.childElementCount;
+    console.log(todoList.childElementCount);
+    completedFunc()
+        
+    
 }
 
-function delMarked(){
+function delMarked() {
     const checkboxes = document.querySelectorAll('input[type=checkbox]:checked');
     console.log(checkboxes)
-    checkboxes.forEach( checkbox => {
+    checkboxes.forEach(checkbox => {
         // console.log(checkbox);
         checkbox.parentNode.remove();
         // console.log(checkbox.parentNode);
-        
+        total.innerText = todoList.childElementCount;
+        console.log(todoList.childElementCount);
+        completedFunc()
     })
 }
-function deleteAll(){
+function deleteAll() {
     const checkboxes = document.querySelectorAll('li');
     console.log(checkboxes)
-    checkboxes.forEach( checkbox => {
+    checkboxes.forEach(checkbox => {
         checkbox.remove();
+        total.innerText = todoList.childElementCount;
+        console.log(todoList.childElementCount);
+        // const checked = document.querySelectorAll('input[type=checkbox]:checked');
+            // completed.innerText = checked.length;
+            completedFunc()
     })
 }
 
-const filterButton = document.querySelector('.filter_button');
-function showfilterBtn(){
-            filterButton.style.visibility='visible';
+function completedFunc(){
+    
+            const checked = document.querySelectorAll('input[type=checkbox]:checked');
+            completed.innerText = checked.length;
+               console.log(checked);
+            console.log(checked.length);
+        
+    
 }
 
-function hidefilterBtn(){
-        filterButton.style.visibility='hidden';
-}
+// const filterButton = document.querySelector('.filter_button');
+// function showfilterBtn(){
+//             filterButton.style.visibility='visible';
+// }
 
+// function hidefilterBtn(){
+//         filterButton.style.visibility='hidden';
+// }
 
 function marked() {
     const checkboxes = document.querySelectorAll('input[type=checkbox]');
-    checkboxes.forEach( checkbox => {
-        if(!checkbox.checked){
+    checkboxes.forEach(checkbox => {
+        if (!checkbox.checked) {
             todoList.removeChild(checkbox.parentElement);
         }
     })
 }
+
 function unmarked() {
     const checkboxes = document.querySelectorAll('input[type=checkbox]');
-    checkboxes.forEach( checkbox => {
-        if(checkbox.checked){
+    checkboxes.forEach(checkbox => {
+        if (checkbox.checked) {
             todoList.removeChild(checkbox.parentElement);
         }
     })
 }
+
